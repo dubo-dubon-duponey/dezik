0e0254c4e5e61d722651cd37b3cd30613666c288
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2016-06-15 09:56:09 -0700

    Fix schemafield no id regression
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- app/adapters/schemafield.js
+++ app/adapters/schemafield.js
@@ -2,6 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Adapter.extend({
 
+  generateIdForRecord: function(store, inputProperties) {
+    console.warn('com.spacedog.tsygan::schemaField->generateIdForRecord', inputProperties);
+    return SpaceDogEngine.uuid();
+  },
+
   deleteRecord: function(){
     console.warn('com.spacedog.tsygan::schemaField->deleteRecord');
     return new Ember.RSVP.Promise(function (resolve/*, reject*/) {

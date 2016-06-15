6171ed43033e789e6f8c5cb133aa44a62e01e047
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2016-06-15 14:15:30 -0700

    Cleanup + poorman solution to updates collapse
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -24,25 +24,20 @@ export default Controller.extend({
 
     // Add a new field
     newField(model) {
-      console.warn('newField------>');
-      window.BLA = this.store;
       const currentFields = model.get('fields');
 
       currentFields.then((data) => {
         const newField = this.store.createRecord('schemafield', {
-//          id: '',
           parentModel: model,
           type: 'string'
         });
 
         data.addObject(newField);
-        // model.save();
       });
     },
 
     // Remove a model
     removeModel(model) {
-      console.warn('removeModel------>');
       const fields = model.get('fields');
 
       // iterate through fields and delete those records
@@ -54,19 +49,12 @@ export default Controller.extend({
 
       model.destroyRecord();
 
-      // trigger an update of the json,etc.
-      // this does a mock ajax call or something, so it needs a timeout to work correctly
-      Ember.run.later(() => {
-        // getModelInfo(this);
-      }, 100);
     },
 
     // Triggered when fields are updated
     updateFields(modelId) {
       var model = this.store.peekRecord('schema', modelId);
-      Ember.run.once(this, function(){
-        model.save();
-      });
+      model.save();
     }
   }
 });

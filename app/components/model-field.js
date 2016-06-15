5ff0dbe2f744d80b0a72cae0275c27e59179937a
dubo-dubon-duponey <farcloser.root@gmail.com>
2016-06-15 14:15:30 -0700

    Cleanup + poorman solution to updates collapse
    Signed-off-by: dubo-dubon-duponey <farcloser.root@gmail.com>

--- app/components/model-field.js
+++ app/components/model-field.js
@@ -2,12 +2,25 @@ import Ember from 'ember';
 
 const { Component, observer } = Ember;
 
+var interval;
+
+var setInter = function(cbk){
+  // If there was something already, cancel and reset
+  if (interval)
+    clearTimeout(interval);
+  interval = setTimeout(cbk, 1000);
+};
+
+
 export default Component.extend({
   typeOptions: SpaceDogEngine.transforms.type.dump(),
   languageOptions: SpaceDogEngine.languageOptions,
   field: null,
-  changeObserver: observer('field.name', 'field.type', 'field.array', 'field.language', 'field.required', 'field.defaultValue', 'field.relatedTo', function () {
-    this.sendAction('update', this.get('field.parentModel.id'));
+  changeObserver: observer('field.name', 'field.type', 'field.array', 'field.language', 'field.required', 'field.defaultValue', 'field.enumSet', 'field.relatedTo', function () {
+    // Schedule an update dispatch
+    setInter(function(){
+      this.sendAction('update', this.get('field.parentModel.id'));
+    }.bind(this));
   }),
 
   actions: {
